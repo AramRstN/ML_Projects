ba72cbeac9ea5733f46fe41de565a277002aba0c
Aram Rostamnejad <maede.rostamnejad@gmail.com>
2025-01-08 17:04:27 +0100

    predict  a real-world problem using regression

---
+++ HousePricePrediction.py
@@ -0,0 +1,35 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+housing = fetch_california_housing()
+X, y = housing.data, housing.target
+
+feature_names = housing.feature_names
+print("Feature names:", feature_names)
+
+#data prepration
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+#Linear Regression
+model = LinearRegression()
+model.fit(X_train_scaled, y_train)
+
+y_pred = model.predict(X_test_scaled)
+
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+print(f"Test RMSE: {rmse:.2f}")
+
+#Extract feature importance
+
+coefficients = model.coef_
+for name, coef in zip(feature_names, coefficients):
+    print(f"{name}: {coef:.3f}")
+
