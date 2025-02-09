b6cd378a1948459591982b4d49ddb1fa4733b34f
Aram Rostamnejad <maede.rostamnejad@gmail.com>
2025-02-09 12:00:48 +0100

    dr_comparing

--- dimension_reduction.py
+++ dimension_reduction.py
@@ -1,6 +1,8 @@
 from sklearn.datasets import load_digits
 from sklearn.decomposition import *
-from matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from sklearn.manifold import *
+from sklearn.svm import *
 
 data = load_digits()
 print(data.shape)
@@ -13,4 +15,19 @@ pca = PCA(2)
 transported_data = pca.fit_transform(data.data)
 
 plt.scatter(transported_data[:,0],transported_data[:,1], c = data.target, cmap = "tab10", s = 4)
+
+#1
+
+def visualize_dim_reduction(name, algorithm, data):
+    alg = algorithm(2)
+    transported_data = pca.fit_transform(data.data)
+
+    plt.scatter(transported_data[:,0],transported_data[:,1], c = data.target, cmap = "tab10", s = 4)
+    plt.title(name)
+    plt.show()
+
+dr_algorithms = {"PCA": PCA, "KernelPCA":KernelPCA, "MiniBatchSparsePCA": MiniBatchSparsePCA, "FastICA": FastICA, "LatentDirichletAllocation": LatentDirichletAllocation, "tSNE": TSNE, "MDS": MDS}
+
+for name, algorithm in dr_algorithms.items():
+    visualize_dim_reduction(name, algorithm, data)
 
